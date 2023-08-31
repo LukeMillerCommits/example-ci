ff5217a52be79b50f0a9d153cd9f8abe4b6fbd45
LukeMillerCommits <lukemiller6024@gmail.com>
2023-08-31 11:48:14 +0800

    Attempt at raising an out of bounds error for fahrenheit to celcius

--- functions.py
+++ functions.py
@@ -8,4 +8,7 @@ def multiply(a, b):
     return a * b
 
 def convert_fahrenheit_to_celsius(fahrenheit):
+    if (fahrenheit < -459.67):
+        raise ValueError("Tempurature cannot be lower than -459.67 fahrenheit")
+    
     return multiply(subtract(fahrenheit, 32), 5 / 9) # <-- Fix this in step 7
